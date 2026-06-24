7662c06e795ae1b2eadb5ba5d4112568d2eff12c
Ayush Chakrani <ayushchakrani@gmail.com>
2026-06-24 16:22:23 +0000

    more about variables

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,11 @@
+let state;
+// number => 2 to power 53
+// bigint
+//string => IIII
+// boolean => true/false
+// null => standalone value
+// undefir There are no staged changes to commit.
+// symbol Would you like to stage all your changes and commit them directly?
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
