5722513cb36c5b95dc5d71ed983085e4458e6ca5
Ayush Chakrani <ayushchakrani@gmail.com>
2026-06-27 16:22:45 +0000

    datatypes

---
+++ 01_basics/datatypes-summarys.js
@@ -0,0 +1,26 @@
+// Primitive 
+// 7 types : String , NUmber, Boolean, Null, Undefined, Symbol, BigInt
+// const score = 100
+const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail 
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id = anotherId)
+// const bigNumber = 63871648564n
+
+// Reference (Non primitive)
+//Array , Objects ,FIncations
+
+const heros = ["shaktiman", "naagaraj", "doga"]
+let myObj = {
+    name: "ayush",
+    age: 22,
+}
+
+const myFucntion = function(){
+    console.log("Hello world")
+}
+console.log(typeof scoreValue)
