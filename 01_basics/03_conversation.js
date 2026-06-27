20c5aeebfdcf5964c1344ff9b43ecdf8a03fa261
Ayush Chakrani <ayushchakrani@gmail.com>
2026-06-27 15:30:36 +0000

    conversation-opration

--- 01_basics/03_conversation.js
+++ 01_basics/03_conversation.js
@@ -1,3 +1,17 @@
 // let score = "33"
-console.log("typeof score");
-// console.log(typeof(score));
+// console.log("typeof score");
+// console.log(typeof(score)); opration
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+console.log("2" == 2)
+// console.log("2" > 1);
+// console.log("02" > 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//
+console.log("2" === 2);
