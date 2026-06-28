5a53e1d0feb46dd9268860ccadf16992b8cfa532
Ayush Chakrani <ayushchakrani@gmail.com>
2026-06-28 16:58:38 +0000

    math&numbers

---
+++ 01_basics/06_maths.js
@@ -0,0 +1,25 @@
+// const score = 400
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3))
+// console.log(hundreds.toLocaleString('en-IN'));
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.2));
+
+console.log(Math.random());
+
+
+const min = 10
+const max = 20
+
+console.log(Math.random() * (max - min + 1))
+
