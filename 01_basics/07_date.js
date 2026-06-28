55c56aed46305839ab25b99f52b63e23b551ad84
Ayush Chakrani <ayushchakrani@gmail.com>
2026-06-28 17:25:09 +0000

    date&time

---
+++ 01_basics/07_date.js
@@ -0,0 +1,12 @@
+// let myDate =new Date()
+// console.log(myDate.toString())
+// let myCreatedDate = new Date("01-14-2023")
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
