7377e48619f4e3c8d44e3364d4737e7241947ccd
Ayush Chakrani <ayushchakrani@gmail.com>
2026-06-29 16:04:49 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+// //arrays
+// const myArr = [0, 2, 3, 5, 6 ]
+// const myHeros = ["shaktian", "nagaraj"]
+
+// const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr)
+
+// myArr.push(6)
+// console.log("push",myArr)
+
+// myArr.pop(6)
+// console.log("pop",myArr)
+
+// myArr.shift()
+// myArr.shift()
+// console.log("shift",myArr)
