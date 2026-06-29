a329134af27b3a46903e7ee8b7f9f79a3cf19e5a
Ayush Chakrani <ayushchakrani@gmail.com>
2026-06-29 16:29:59 +0000

    objects

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,14 @@
+//singlton
+//object literal
+const myObj = {
+    name: 'John',
+    age: 30,
+    location: 'New York',
+    email: 'ayush@google.com',
+    isLoggedIn: true,
+    lastLoginDays: ['Monday', 'Friday']
+}
+
+console.log(myObj.email)
+// console.log(myObj.["email"])
+
