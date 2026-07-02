d3749cc8614edc9edc71447c13d54508c696f7bf
Ayush Chakrani <ayushchakrani@gmail.com>
2026-07-02 17:17:12 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+
+function sayMyName() {
+  console.log("A");
+  console.log("B");
+  console.log("C");
+  console.log("D");
+  console.log("E");
+}
+
+sayMyName();
+
+function addTwoNumbers(num1, num2) {
+  console.log(num1 + num2);
+}
+
+addTwoNumbers(5, 10);
