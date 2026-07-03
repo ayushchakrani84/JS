67c9b4cc9cd70151aa4546bcb38cccf7b9d0815e
Ayush Chakrani <ayushchakrani@gmail.com>
2026-07-03 04:13:54 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,16 +1,39 @@
 
-function sayMyName() {
-  console.log("A");
-  console.log("B");
-  console.log("C");
-  console.log("D");
-  console.log("E");
+// function sayMyName() {
+//   console.log("A");
+//   console.log("B");
+//   console.log("C");
+//   console.log("D");
+//   console.log("E");
+// }
+
+// sayMyName();
+
+// function addTwoNumbers(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// addTwoNumbers(5, 10);
+
+
+function calculateCartPrice(val1, val2, ...num1){
+  return num1
+}
+console.log(calculateCartPrice(200, 400, 500, 2000)); 
+
+const user = {
+  userName: "John",
+  price : 199
 }
 
-sayMyName();
-
-function addTwoNumbers(num1, num2) {
-  console.log(num1 + num2);
+function handleObject(anyObject){
+  console.log(`Username is ${anyObject.userName} and price is ${anyObject.price}`);
 }
 
-addTwoNumbers(5, 10);
+handleObject(user);
+
+function returnSecondValue(getArray) {
+  return getArray[1]
+}
+//console.log(returnSecondValue (myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
