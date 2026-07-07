968f68b5616575684b38588319d7d7388916d2f0
Ayush Chakrani <ayushchakrani@gmail.com>
2026-07-07 17:01:53 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,11 +1,26 @@
+// let a = 300
+// // var c = 300
+// if (true){
+//     let a = 10
+//     const b = 20
+//     console.log("INNER: ", a);
+// }
 
-// var c = 300
-if (true){
-    let a = 10
-    const b = 20
-    var c = 30
-}
+// for (let i = 0; i < 5; i++){
+//     console.log("INNER FOR: ", i);
+// }
 
+// // console.log(a)
+// // console.log(b)
 // console.log(a)
-// console.log(b)
-console.log(c)
+function one(){    
+    const username = "ayush"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
