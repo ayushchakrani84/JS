fa9d8859a1d50a5c0e8bfc5247cfb037b9309319
Ayush Chakrani <ayushchakrani@gmail.com>
2026-07-10 16:09:34 +0000

    arrow

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,24 @@
+// const user = {
+//     username: "ayush",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+        
+//     }
+// }
+
+// user.welcomeMessage();
+// user.username = "ayush kumar";
+// user.welcomeMessage();
+// console.log(this);
+//  function chai(){
+//     let username = "hitesh"
+//     console.log(this);
+    
+//  } 
+//  chai()
+const addTwo = (a, b) => a + b
+
+console.log(addTwo(2, 3));
