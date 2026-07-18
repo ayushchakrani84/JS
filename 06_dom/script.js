83c49cc2ac17a495fa9d2192b4ab9534dd97a0fc
Ayush Chakrani <ayushchakrani@gmail.com>
2026-07-18 12:00:52 +0000

    dom-2

--- 06_dom/script.js
+++ 06_dom/script.js
@@ -1,6 +1,18 @@
-let button = document.getElementById('myid');
-console.dir(button);
+// let button = document.getElementById('myid');
+// console.dir(button);
 
-let headings = document.getElementsByClassName('heading-class');
-console.dir(headings);
-console.log(headings);
+// let headings = document.getElementsByClassName('heading-class');
+// console.dir(headings);
+// console.log(headings);
+
+// let div = document.querySelector("div");
+
+// div.style.backgroundColor = "green";
+// div.style.color = "white";
+// div.style.visibility = "Animation";
+
+// div.style.fontSize = "30px";
+// div.innerText = "helo";
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click me!";
+document.body.appendChild(newBtn);  
